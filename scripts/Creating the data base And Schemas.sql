a418c8d08c80b704350ccdabb3f159b318c3a14d
Amaranatha H <amaranathahcse6@gmail.com>
2026-09-03 10:43:23 +0530

    Add files via upload

---
+++ scripts/Creating the data base And Schemas.sql
@@ -0,0 +1,27 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'
+*/
+
+
+
+-- Create Database 'DataWarehouse'
+
+-- CREATE DATABASE DataWarehouse
+
+USE DataWarehouse;
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA sliver;
+GO
+
+CREATE SCHEMA gold;
+GO
+
